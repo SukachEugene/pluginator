09917f25fb3f8d4029b3844b0ed2732ab53a759d
Yevhenii Sukach <sukachy@digicode.ua>
2023-05-03 09:33:14 +0300

    save in progress

--- wp-content/plugins/eugene-plugin/inc/Pages/Admin.php
+++ wp-content/plugins/eugene-plugin/inc/Pages/Admin.php
@@ -7,23 +7,80 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
 
+   public $settings;
+   public $pages = array();
+   public $subpages = array();
+
+
    public function register()
    {
-      add_action('admin_menu', array($this, 'add_admin_pages'));
+
+      $this->settings = new SettingsApi;
+
+      $this->setPages();
+
+      $this->setSubPages();
+
+      $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
    }
 
-   public function add_admin_pages()
-   {
-      add_menu_page('Eugene Plugin', 'Eugene Plugin', 'manage_options', 'eugene_plugin', 
-      array($this, 'admin_index'), 'dashicons-editor-customchar', 110);
+   public function setPages() {
+
+      $this->pages = array(
+         array(
+            'page_title' => 'Eugene Plugin',
+            'menu_title' => 'Eugene Plugin',
+            'capability' => 'manage_options',
+            'menu_slug'  => 'eugene_plugin',
+            'callback'   =>  function () { return require_once("$this->plugin_path/templates/admin.php");},
+            'icon_url'   => 'dashicons-editor-customchar',
+            'position'   => 110
+         )
+
+      );
    }
 
-   public function admin_index()
-   {
-      require_once $this->plugin_path . 'templates/admin.php';
+   public function setSubPages(){
+
+      $this->subpages = array(
+         array(
+            'parent_slug' => 'eugene_plugin',
+            'page_title'  => 'Custom Post Types',
+            'menu_title'  => 'CPT',
+            'capability'  => 'manage_options',
+            'menu_slug'   => 'eugene_cpt',
+            'callback'    => function () {
+               echo '<h1>CPT Manager</h1>';
+            },
+         ),
+
+         array(
+            'parent_slug' => 'eugene_plugin',
+            'page_title'  => 'Custom Taxonomies',
+            'menu_title'  => 'Taxonomies',
+            'capability'  => 'manage_options',
+            'menu_slug'   => 'eugene_taxonomies',
+            'callback'    => function () {
+               echo '<h1>Taxonomies Manager</h1>';
+            },
+         ),
+
+         array(
+            'parent_slug' => 'eugene_plugin',
+            'page_title'  => 'Custom Widgets',
+            'menu_title'  => 'Widgets',
+            'capability'  => 'manage_options',
+            'menu_slug'   => 'eugene_widgets',
+            'callback'    => function () {
+               echo '<h1>Widgets Manager</h1>';
+            },
+         ),
+      );
+
    }
 }
