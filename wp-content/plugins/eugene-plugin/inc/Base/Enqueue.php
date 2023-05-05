b62a4b62bf8faefcff3ea77798becc5dafefe644
Yevhenii Sukach <sukachy@digicode.ua>
2023-05-05 19:04:52 +0300

    save in progress after lesson 30

--- wp-content/plugins/eugene-plugin/inc/Base/Enqueue.php
+++ wp-content/plugins/eugene-plugin/inc/Base/Enqueue.php
@@ -22,7 +22,10 @@ class Enqueue extends BaseController
     {
         // enqueue all our scripts
         wp_enqueue_style('mystyle', $this->plugin_url . 'assets/mystyle.css');
-        wp_enqueue_style('checkbox', $this->plugin_url . 'assets/scss/checkbox.css');
+        wp_enqueue_style('checkbox', $this->plugin_url . 'assets/scss/checkbox/checkbox.css');
+        wp_enqueue_style('table', $this->plugin_url . 'assets/scss/table/table.css');
+        wp_enqueue_style('tabs', $this->plugin_url . 'assets/scss/tabs/tabs.css');
+        wp_enqueue_style('form', $this->plugin_url . 'assets/scss/form/form.css');
         wp_enqueue_script('myscript', $this->plugin_url . 'assets/myscript.js');
     }
 }
